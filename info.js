ac4b6f22830c6f93247a4ba6c0e612d51fb77e3a
Aufanino <74483846+Aufanino@users.noreply.github.com>
2020-11-15 13:23:06 +0700

    Add files via upload

---
+++ info.js
@@ -0,0 +1,35 @@
+exports.info = (id, BotName, corohelp, tampilTanggal, tampilWaktu, instagram, telegram, youtube, kapanbotaktif, grupch1, grupch2) => {
+	return `⚜ ${BotName} ⚜
+  
+ʜᴀʟʟᴏ *${id.split("@s.whatsapp.net")[0]}*
+"
+Pasang Iklan? Chat Whatsapp wa.me/6285726297302
+
+💰ᴅᴏɴᴀsɪ ᴀɢᴀʀ ʙᴏᴛ ᴛᴇᴛᴀᴘ ᴀᴋᴛɪғ, !ᴅᴏɴᴀsɪ ᴜɴᴛᴜᴋ ʙᴇʀᴅᴏɴᴀsɪ.
+
+INFO BOT! :
+🔱 *OWNER*: AUFA NINO 
+🔱 *GITHUB*: https://github.com/
+🔱 *INSTAGRAM*: ${instagram}
+🔱 *YOUTUBE*: ${youtube}
+🔱 *TELEGRAM* : ${telegram}
+🔱 *WA OWNER* : wa.me/6285726297302
+
+INFO LAIN! :
+
+ʙᴏᴛ ɪɴɪ ᴍᴇɴɢɢᴜɴᴀᴋᴀɴ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ʏᴀɴɢ sᴀʏᴀ ᴅᴀᴘᴀᴛᴋᴀɴ ᴅᴀɴ sᴀʏᴀ ᴍᴏᴅɪғɪᴋᴀsɪ ᴅᴇɴɢᴀɴ ᴘᴇɴᴀᴍʙᴀʜᴀɴ, ᴘᴇɴɢᴜʀᴀɴɢᴀɴ, ᴅᴀɴ ᴘᴇʀᴜʙᴀʜᴀɴ ʙᴇʙᴇʀᴀᴘᴀ ғɪᴛᴜʀ.
+ʙᴏᴛ ɪɴɪ ᴅɪʙᴜᴀᴛ ᴍᴇɴɢɢᴜɴᴀᴋᴀɴ ɴᴏᴅᴇ.ᴊs
+ᴄʀᴇᴀᴛᴏʀ ʙʏ ʀᴇᴜᴢ
+
+
+ʙᴏᴛ ᴀᴋᴛɪғ sᴇʟᴀᴍᴀ : ${kapanbotaktif}
+ᴊᴀɴɢᴀɴ ʟᴜᴘᴀ ғᴏʟʟᴏᴡ ɪɴsᴛᴀɢʀᴀᴍ
+ ${instagram}
+
+ ☞ᴛᴇʟᴇɢʀᴀᴍ : ${telegram}`
+╔═╗═════╔╗════╔╗═╔╦╦╗
+║║╠═╦═╦═╬╣╔╦╦╦╣╠╗║║║║
+║║║╬║╬║╬║║║║║║║═╣╠╬╬╣
+╚╩╬╗╠═╣╔╩╝╠╗╠═╩╩╝╚╩╩╝
+══╚═╝═╚╝══╚═╝
+}
